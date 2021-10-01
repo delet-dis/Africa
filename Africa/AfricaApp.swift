2c39255df25983bb120487fc0537cd453686a958
delet-dis <igor@efimov.ga>
2021-10-01 13:08:12 +0700

    ℹ️ Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Igor Efimov on 01.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
