60356efc1917c0adb3f3053cb00fbf0cb1e99c5d
delet-dis <igor@efimov.ga>
2021-10-02 20:46:26 +0700

    📱 added views for tab view

---
+++ Africa/App/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  MainView.swift
+//  Africa
+//
+//  Created by Igor Efimov on 02.10.2021.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView{
+            
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Igor Efimov on 02.10.2021.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
