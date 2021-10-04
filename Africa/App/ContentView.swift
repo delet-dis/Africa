56d2fdfa8fbf236afd448bea7dd839d91da8b041
delet-dis <igor@efimov.ga>
2021-10-04 17:26:32 +0700

    📃 added animal list

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,11 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        let animals:[Animal] = Bundle.main.decode("animals.json")
+        
         NavigationView{
             List{
                 CoverImageView()
                     .frame(height:300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    .padding(.bottom, 5)
+                
+                ForEach(animals){animal in
+                    AnimalListItemView(animal: animal)
+                }
             }
             .navigationBarTitle("Africa", displayMode: .large)
         }
