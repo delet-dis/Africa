26c2228333801d350cbcb149856ff83aaf8d868d
delet-dis <igor@efimov.ga>
2021-10-15 22:57:49 +0700

    📹 added videos list with shuffle

---
+++ Africa/Model/VideoModel.swift
@@ -0,0 +1,18 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by Igor Efimov on 15.10.2021.
+//
+
+import Foundation
+
+struct Video: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+    
+    var thumbnail:String{
+        "video-\(id)"
+    }
+}
