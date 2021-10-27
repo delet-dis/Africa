052e83814958a3c248281d37f6ec8d36abd32293
delet-dis <igor@efimov.ga>
2021-10-27 14:22:38 +0700

    🗺️ added map with national parks

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -5,11 +5,27 @@
 //  Created by Igor Efimov on 02.10.2021.
 //
 
+import MapKit
 import SwiftUI
 
 struct MapView: View {
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+
+        return mapRegion
+    }()
+
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+
     var body: some View {
-        Text("Map")
+        Map(coordinateRegion: $region, annotationItems: locations, annotationContent: {
+            item in
+            MapAnnotation(coordinate: item.location){
+                MapAnnotationView(location: item)
+            }
+        })
     }
 }
 
---
+++ Africa/Model/LocationModel.swift
@@ -0,0 +1,21 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Igor Efimov on 26.10.2021.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
