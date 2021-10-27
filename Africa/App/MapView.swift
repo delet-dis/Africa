acc0205387e183025d36433b6d18cc22035282da
delet-dis <igor@efimov.ga>
2021-10-27 15:02:29 +0700

    🗺️ added map overlay

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -22,10 +22,53 @@ struct MapView: View {
     var body: some View {
         Map(coordinateRegion: $region, annotationItems: locations, annotationContent: {
             item in
-            MapAnnotation(coordinate: item.location){
+            MapAnnotation(coordinate: item.location) {
                 MapAnnotationView(location: item)
             }
         })
+            .overlay(
+                HStack(alignment: .center, spacing: 12) {
+                    Image("compass")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 48, height: 48, alignment: .center)
+                    
+                    VStack(alignment: .leading, spacing:3){
+                        HStack{
+                            Text("Latitude:")
+                                .font(.footnote)
+                                .fontWeight(.bold)
+                                .foregroundColor(.accentColor)
+                            Spacer()
+                            Text("\(region.center.latitude)")
+                                .font(.footnote)
+                                .foregroundColor(.white)
+                        }
+                        
+                        Divider()
+                        
+                        HStack{
+                            Text("Longitude:")
+                                .font(.footnote)
+                                .fontWeight(.bold)
+                                .foregroundColor(.accentColor)
+                            Spacer()
+                            Text("\(region.center.longitude)")
+                                .font(.footnote)
+                                .foregroundColor(.white)
+                        }
+                    }
+                }
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .background(
+                    Color.black
+                        .cornerRadius(8)
+                        .opacity(0.6)
+                )
+                .padding()
+                , alignment: .top
+            )
     }
 }
 
