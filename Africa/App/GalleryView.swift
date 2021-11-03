ac42181779b2f127679fe2cc400b54c5279a926d
delet-dis <igor@efimov.ga>
2021-11-03 23:24:27 +0700

    🖼️ added gallery grid

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct GalleryView: View {
+    @State private var selectedAnimal = "lion"
+    
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    let animals:[Animal] = Bundle.main.decode("animals.json")
+
     var body: some View {
-        ScrollView(.vertical, showsIndicators:false){
-            Text("Gallery")
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing:30) {
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                LazyVGrid(columns:gridLayout, alignment:.center, spacing:10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                    
+                    }
+                }
+            }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         }
-        .frame(maxWidth:.infinity, maxHeight: .infinity)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
     }
 }
